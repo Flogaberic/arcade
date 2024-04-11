42002d382d53602675fe29d9b769d85b9f2d5136
Gabriel Fonollosa <gabriel.fonollosa@apropos.local>
2024-04-11 12:56:42 +0200

    Ajout jeu hors ligne

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -24,19 +24,25 @@ class AccueilController extends AbstractController
         # Remplacer l'id par le numéro du jeu
         $jeu1 = $this->entityManager->getRepository(Jeux::class)->findOneBy(['id' => '1']);
         $img1 = $jeu1 ? $jeu1->getImg() : 'Aucun jeu trouvé';
+        $chemin1 = $jeu1 ? $jeu1->getChemin() : 'Aucun jeu trouvé';
 
         # Remplacer l'id par le numéro du jeu
         $jeu2 = $this->entityManager->getRepository(Jeux::class)->findOneBy(['id' => '2']);
         $img2 = $jeu2 ? $jeu2->getImg() : 'Aucun jeu trouvé';
+        $chemin2 = $jeu2 ? $jeu2->getChemin() : 'Aucun jeu trouvé';
 
         # Remplacer l'id par le numéro du jeu
         $jeu3 = $this->entityManager->getRepository(Jeux::class)->findOneBy(['id' => '3']);
         $img3 = $jeu3 ? $jeu3->getImg() : 'Aucun jeu trouvé';
+        $chemin3 = $jeu3 ? $jeu3->getChemin() : 'Aucun jeu trouvé';
 
         return $this->render('accueil.html.twig', [
             'img1' => $img1,
             'img2' => $img2,
             'img3' => $img3,
+            'chemin1' => $chemin1,
+            'chemin2' => $chemin2,
+            'chemin3' => $chemin3
         ]);
     }
 
---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categories', name: 'categories', methods: ['GET', 'POST'])]
+    public function index(): Response
+    {
+        return $this->render('categories.html.twig');
+    }
+
+    #[Route('/categories.1', name: 'categories.1', methods: ['GET', 'POST'])]
+    public function categorie1(): Response
+    {
+        return $this->render('categories1.html.twig');
+    }
+
+    #[Route('/categories.2', name: 'categories.2', methods: ['GET', 'POST'])]
+    public function categorie2(): Response
+    {
+        return $this->render('categories2.html.twig');
+    }
+}
